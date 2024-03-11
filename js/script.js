ea9c39ec9faf20975535618c8e9003b7f33e866a
RevinZ <zuhdirevin@gmail.com>
2024-03-11 09:02:56 +0700

    membuat website kedai kopi tigasama

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Toggle Class Active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika menu di click
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// click di luar sidebar untuk menghilangkan nav
+
+const menu = document.querySelector("#menu");
+
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
